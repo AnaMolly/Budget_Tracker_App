74a8dea9d272443160c019e1d39ba34b786989fd
AnaMolly <analia.mohamdally@gmail.com>
2021-07-18 19:33:07 -0400

    added db.js file and added to html doc. also fixed errors

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,29 +1,28 @@
-const CACHE_NAME = "static-cache-v2";
-const DATA_CACHE_NAME = "data-cache-v1";
 const FILES_TO_CACHE = [
   "/",
   "/index.html",
-  "index.js",
+  "/index.js",
+  "/db.js",
   "/manifest.webmanifest",
   "/styles.css",
   "/icons/icon-192x192.png",
   "/icons/icon-512x512.png",
 ];
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
+ 
 
-// install
 self.addEventListener("install", function (evt) {
 
-  // pre cache all static assets
   evt.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
   );
 
-  // tell the browser to activate this service worker immediately once it
-  // has finished installing
+ 
   self.skipWaiting();
 });
 
-// activate
+
 self.addEventListener("activate", function(evt) {
   evt.waitUntil(
     caches.keys().then(keyList => {
@@ -41,14 +40,13 @@ self.addEventListener("activate", function(evt) {
   self.clients.claim();
 });
 
-// fetch
+
 self.addEventListener("fetch", function(evt) {
   if (evt.request.url.includes("/api/")) {
     evt.respondWith(
       caches.open(DATA_CACHE_NAME).then(cache => {
         return fetch(evt.request)
           .then(response => {
-            // If the response was good, clone it and store it in the cache.
             if (response.status === 200) {
               cache.put(evt.request.url, response.clone());
             }
@@ -56,7 +54,6 @@ self.addEventListener("fetch", function(evt) {
             return response;
           })
           .catch(err => {
-            // Network request failed, try to get it from the cache.
             return cache.match(evt.request);
           });
       }).catch(err => console.log(err))
--- server.js
+++ server.js
@@ -20,8 +20,7 @@ mongoose.connect("mongodb://localhost/budget", {
   useFindAndModify: false
 });
 
-// routes
-app.use(require("./routes/api.js"));
+app.use(require("./routes/api.js"))
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
